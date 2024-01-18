e6e24c95937ce36d9b4f2530d749cdc61f842b72
Rachid Boujakdji <rachidboudjakdji27@gmail.com>
2024-01-18 14:40:01 +0100

    affichage de la fenêtre avec sauce + valider la commande

--- src/pages/recettes.jsx
+++ src/pages/recettes.jsx
@@ -4,6 +4,9 @@ import { useState, useEffect } from 'react';
 
 export default function recettes() {
     const [recettes, setRecettes] = useState([]);
+    const [sauceVisible, setSauceVisible] = useState(false);
+    const [recetteSelect, setRecetteSelect] = useState(null);
+    const [sauceSelect, setSauceSelect] = useState('algérienne');
 
     useEffect(() => {
         const recettesExistantes = JSON.parse(localStorage.getItem('recettes')) || [];
@@ -16,6 +19,22 @@ export default function recettes() {
         localStorage.setItem('recettes', JSON.stringify(nouvellesRecettes));
     };
 
+    const affichSauce = (recette) => {
+        setRecetteSelect(recette);
+        setSauceVisible(true);
+    };
+
+    const fermerSauce = () => {
+        setSauceVisible(false);
+        setRecetteSelect(null);
+        setSauceSelect('algérienne');
+    };
+
+    const validerCommande = async () => {
+        console.log('Commande validée:', recetteSelect, sauceSelect);
+        fermerSauce();
+    };
+
     return (
         <div className='recette'>
             <h2>Nos recettes</h2>
@@ -25,13 +44,29 @@ export default function recettes() {
                         <h3>{recette.nom}</h3>
                         <p>{recette.ingredients}</p>
                         <div className="btn-recette">
-                            <button className='btn'>Commander</button>
+                            <button className='btn' onClick={() => affichSauce(recette)}>Commander</button>
                             <button className='btn-suppr' onClick={() => supprimerRecette(index)}>Supprimer</button>
                         </div>
                     </div>
                 ))}
             </div>
 
+            {sauceVisible && (
+            <div className="container-sauce">
+                <div className='close-sauce'>
+                        <p>Sélectionnez une sauce avec {recetteSelect?.nom}</p>
+                    <span className="close" onClick={fermerSauce}>&times;</span>
+                </div>
+                <div className="choix-sauce">
+                    <select value={sauceSelect} onChange={(e) => setSauceSelect(e.target.value)}>
+                        <option value="algérienne">Sauce Algérienne</option>
+                        <option value="barbecue">Sauce Barbecue</option>
+                        <option value="aucune">Aucune sauce</option>
+                    </select>
+                    <button className='btn2' onClick={validerCommande}>Valider la commande</button>
+                </div>
+            </div>
+            )}
         </div>
     );
 }
