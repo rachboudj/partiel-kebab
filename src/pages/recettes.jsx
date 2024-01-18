95828a0d569869c4e5141ec9e3d228f3ebfd04d8
Rachid Boujakdji <rachidboudjakdji27@gmail.com>
2024-01-18 11:52:47 +0100

    affichage des recettes sur la page

--- src/pages/recettes.jsx
+++ src/pages/recettes.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import { useState, useEffect } from 'react';
+
 
 export default function recettes() {
+    const [recettes, setRecettes] = useState([]);
+
+    useEffect(() => {
+        const recettesExistantes = JSON.parse(localStorage.getItem('recettes')) || [];
+        setRecettes(recettesExistantes);
+    }, []);
+
     return (
         <div>
-            <h1>Recette</h1>
+            <h2>Nos recettes</h2>
+            {recettes.map((recette, index) => (
+                <div key={index}>
+                    <h3>{recette.nom}</h3>
+                    <p>{recette.ingredients}</p>
+                </div>
+            ))}
         </div>
-    )
+    );
 }
