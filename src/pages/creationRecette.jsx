7a473cecf280e67daa4bd54a4c2b7120b60ca25f
Rachid Boujakdji <rachidboudjakdji27@gmail.com>
2024-01-18 11:39:20 +0100

    ajout d'une recette, affichage dans la console

--- src/pages/creationRecette.jsx
+++ src/pages/creationRecette.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import React from 'react';
+import { useState } from 'react';
+
 
 export default function creationRecette() {
-    return (
-        <div>
-            <h1>Créer une recette</h1>
-        </div>
-    )
-}
+    const [nomRecette, setNomRecette] = useState('');
+    const [ingredients, setIngredients] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const recette = {
+            nom: nomRecette,
+            ingredients: ingredients.split(',')
+        };
+
+        console.log('La recette :', recette);
+
+        setNomRecette('');
+        setIngredients('');
+    };
+
+
+
+return (
+
+
+    <div>
+        <h2>Créer une nouvelle recette</h2>
+        <form onSubmit={handleSubmit}>
+            <div>
+                <label>Nom de la recette:</label>
+                <input
+                    type="text"
+                    value={nomRecette}
+                    onChange={(e) => setNomRecette(e.target.value)}
+                    required
+                />
+            </div>
+            <div>
+                <label>Ingrédients (séparés par une virgule):</label>
+                <input
+                    type="text"
+                    value={ingredients}
+                    onChange={(e) => setIngredients(e.target.value)}
+                    required
+                />
+            </div>
+            <button type="submit">Ajouter la recette</button>
+        </form>
+    </div>
+)
+    }
